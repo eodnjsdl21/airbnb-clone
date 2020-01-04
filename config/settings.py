29d2727cedc7511f43c2165a14b04d428809d5ca
unknown <eodnjsdl21@naver.com>
2020-01-04 12:41:09 +0900

    2020-01-04

--- config/settings.py
+++ config/settings.py
@@ -39,8 +39,12 @@ DJANGO_APPS = [
     'django.contrib.staticfiles',
 ]
 
+THIRD_PARTY_APPS = [
+    "django_countries"
+]
+
 PROJECT_APPS = [
-    "users.apps.UsersConfig",
+    "core.apps.CoreConfig", "users.apps.UsersConfig", "rooms.apps.RoomsConfig", "reviews.apps.ReviewsConfig", "reservations.apps.ReservationsConfig", "lists.apps.ListsConfig", "conversations.apps.ConversationsConfig"
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
@@ -126,3 +130,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 AUTH_USER_MODEL = "users.User"
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
+
+MEDIA_URL = "/media/"
